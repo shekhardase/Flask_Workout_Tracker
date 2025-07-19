b23a0ea8f32317e34b0222f420a7c9960156282b
Shekhar <115777486+shekhardase@users.noreply.github.com>
2025-07-19 20:09:01 +0530

    First Flask Project

---
+++ Backend/app.py
@@ -0,0 +1,43 @@
+
+import pymongo
+import os
+from dotenv import load_dotenv
+from flask import Flask, request
+
+
+load_dotenv()
+
+MONGO_URI = os.getenv('MONGO_URI')
+
+client = pymongo.MongoClient(MONGO_URI)
+db = client.test
+collection = db['flask-tutorial']
+
+
+app = Flask(__name__)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = dict(request.json)
+    collection.insert_one(data)
+    return 'Data submitted nicely'
+
+
+@app.route('/view')
+def view():
+    data = collection.find()
+    data = list(data)
+    for i in data:
+        print(i)
+        del i['_id']
+    # # return data
+
+    data = {
+        'data': data
+    }
+    return data
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000, debug=True)
---
+++ Frontend/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+
+BACKEND_URL = 'http://127.0.0.1:9000'
+
+
+@app.route('/')
+def home():
+    day_of_week = datetime.today().strftime("%A")
+    currentTime = datetime.now().strftime("%H:%M:%S")
+    return render_template('index.html', day_of_week=day_of_week, currentTime=currentTime)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = dict(request.form)
+
+    requests.post(BACKEND_URL + '/submit', json=data)
+    return 'Data submitted nicely'
+
+
+@app.route('/get_data')
+def get_data():
+    respose = requests.get(BACKEND_URL + '/view')
+    # print(respose.json)
+    return respose.json()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
